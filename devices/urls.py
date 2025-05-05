5a487cfcfc494641d8ad46c528252da3bfb95002
Nawaz07 <nawaz@pointpi.com>
2025-05-05 14:11:32 +0530

    user login views and urls created

--- devices/urls.py
+++ devices/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from .views.devices_views import device_detail, device_list
-from .views.user_views import register_user
+from .views.user_views import register_user, login_user
 
 urlpatterns = [
     path('devices/',device_list, name='device-list'),
     path('devices/<int:pk>/',device_detail, name='device-detail'),
     path('register/', register_user, name='register-user'),
+    path('login/', login_user, name='login-user'),
 ]
--- devices/views/user_views.py
+++ devices/views/user_views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from django.contrib.auth.models import User
 from rest_framework.authtoken.models import Token
+from django.contrib.auth import authenticate
 
 @api_view(['POST'])
 def register_user(request):
@@ -23,3 +24,18 @@ def register_user(request):
     token = Token.objects.create(user=user)
     
     return Response({"message":"User Created","token": token.key}, status=status.HTTP_201_CREATED)
+
+@api_view(['POST'])
+def login_user(request):
+    username = request.data.get('username')
+    password = request.data.get('password')
+
+    if not username and not password:
+        return Response({"Error":"username and password required"}, status=status.HTTP_400_BAD_REQUEST)
+    user = authenticate(username=username, password=password)
+
+    if not user:
+        return Response({"error":"Invalid user credentials"}, status=status.HTTP_401_UNAUTHORIZED)
+    
+    token,_= Token.objects.get_or_create(user=user)
+    return Response({'massage':"login seccessfully", "token":token.key})
