11ab4a76c0bda0b14c77c2d95558b06a22978e72
Nawaz07 <nawaz@pointpi.com>
2025-05-05 14:31:59 +0530

    user logout

--- devices/views/user_views.py
+++ devices/views/user_views.py
@@ -1,4 +1,5 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
 from django.contrib.auth.models import User
@@ -39,3 +40,9 @@ def login_user(request):
     
     token,_= Token.objects.get_or_create(user=user)
     return Response({'massage':"login seccessfully", "token":token.key})
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def logout_user(request):
+    request.user.auth_token.delete()
+    return Response({"message":"Logged out successfully"}, status=status.HTTP_200_OK)
